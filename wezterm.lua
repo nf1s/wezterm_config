0ffb2f4f1381ee9ca0193770750e9ecdd4d6b3e9
Ahmed Nafies <ahmed.nafies@gmail.com>
2024-02-06 21:11:12 +0100

    fix: fixes cursor

--- wezterm.lua
+++ wezterm.lua
@@ -4,14 +4,18 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
+config.term = "xterm-256color"
 config.color_scheme = 'rose-pine-moon'
 
 -- font
 config.font = wezterm.font('Hasklug Nerd Font Mono', { weight = 'DemiBold' })
 config.font_size = 15.0
 
--- and finally, return the configuration to wezterm
+-- cursor
+config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_rate = 800
+
+-- window
+config.enable_tab_bar = false
+
 return config
