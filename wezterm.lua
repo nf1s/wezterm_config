5f88273cbc6780e2dd5db404d0de0f8625cb6735
Ahmed Nafies <ahmed.nafies@gmail.com>
2024-02-08 08:40:40 +0100

    feat: adds key bindings

--- wezterm.lua
+++ wezterm.lua
@@ -2,20 +2,21 @@
 local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
-local config = wezterm.config_builder()
+return {
 
-config.term = "xterm-256color"
-config.color_scheme = 'rose-pine-moon'
+term = "xterm-256color",
+color_scheme = 'rose-pine-moon',
+keys = {
+    {key="-", mods="CTRL", action="DisableDefaultAssignment"},
+    {key="+", mods="CTRL", action="DisableDefaultAssignment"},
+},
 
--- font
-config.font = wezterm.font('Hasklug Nerd Font Mono', { weight = 'DemiBold' })
-config.font_size = 15.0
+font = wezterm.font('Hasklug Nerd Font Mono', { weight = 'DemiBold' }),
+font_size = 15.0,
 
--- cursor
-config.default_cursor_style = "BlinkingBar"
-config.cursor_blink_rate = 800
+default_cursor_style = "BlinkingBar",
+cursor_blink_rate = 800,
 
--- window
-config.enable_tab_bar = false
+enable_tab_bar = false,
 
-return config
+}
