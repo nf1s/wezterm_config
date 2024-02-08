b004958bda2d121e2e2100bbb75fca2116c7b970
Ahmed Nafies <ahmed.nafies@gmail.com>
2024-02-08 15:38:19 +0100

    refactor: formats

--- wezterm.lua
+++ wezterm.lua
@@ -3,24 +3,20 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 return {
+    term = "xterm-256color",
+    color_scheme = 'rose-pine-moon',
+    keys = {
+        {key="-", mods="CTRL", action="DisableDefaultAssignment"},
+        {key="+", mods="CTRL", action="DisableDefaultAssignment"},
+    },
+    font = wezterm.font('Hasklug Nerd Font Mono', { weight = 'DemiBold' }),
+    font_size = 15.0,
 
-term = "xterm-256color",
-color_scheme = 'rose-pine-moon',
-keys = {
-    {key="-", mods="CTRL", action="DisableDefaultAssignment"},
-    {key="+", mods="CTRL", action="DisableDefaultAssignment"},
-},
-
-font = wezterm.font('Hasklug Nerd Font Mono', { weight = 'DemiBold' }),
-font_size = 15.0,
-
-default_cursor_style = "BlinkingBar",
-cursor_blink_rate = 800,
-colors = {
-    selection_fg = "#ffffff",
-    selection_bg = "#4d4d4d",
-},
-
-enable_tab_bar = false,
-
+    default_cursor_style = "BlinkingBar",
+    cursor_blink_rate = 800,
+    colors = {
+        selection_fg = "#ffffff",
+        selection_bg = "#4d4d4d",
+    },
+    enable_tab_bar = false,
 }
